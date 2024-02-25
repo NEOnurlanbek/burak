1f0dce5e99c248f2f8f5f3b039bc4b455dc113ad
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-02-25 17:39:46 +0900

    feat: integretion of environmental vareiables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
-console.log("ishga tushir");
+import dotenv from 'dotenv';
+dotenv.config();
 
-
-import moment from "moment";
-
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-const person: string = "Martin";
-const count: number = 49;
+console.log("PORT:",process.env.PORT);
+console.log("MONGO_URL:",process.env.PORT);
