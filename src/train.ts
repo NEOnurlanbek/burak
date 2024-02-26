2ed97ba6ba48fa1b6c6cfca670478f8e5b0e27c4
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-02-26 16:57:20 +0900

    feat: H - task

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,9 @@
 console.log("ishga tushdi tushyapti");
+
+function getReverse(str: string): string {
+    return str.split('').reverse().join('');
+}
+
+const originalString: string = "hello";
+const reversedString: string = getReverse(originalString);
+console.log(reversedString); 
