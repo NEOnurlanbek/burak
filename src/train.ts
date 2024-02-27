8f238a7f9d6cfe4b441d96543ca57f95c06608c9
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-02-27 22:43:31 +0900

    feat: H - task

--- src/train.ts
+++ src/train.ts
@@ -1,9 +1,31 @@
-console.log("ishga tushdi tushyapti");
+ console.log("ishga tushdi tushyapti");
+// H - task
 
-function getReverse(str: string): string {
-    return str.split('').reverse().join('');
+function getPositive(numbers: number[]): string {
+    const positiveNumbers = numbers.filter(num => num > 0);
+    const positiveString = positiveNumbers.join('');
+
+    return positiveString;
 }
 
-const originalString: string = "hello";
-const reversedString: string = getReverse(originalString);
-console.log(reversedString); 
+const inputArray: number[] = [1, -4, 2];
+const result: string = getPositive(inputArray);
+console.log(result); 
+
+
+
+
+
+
+
+
+
+
+
+ // function getReverse(str: string): string {
+//     return str.split('').reverse().join('');
+// }
+
+// const originalString: string = "hello";
+// const reversedString: string = getReverse(originalString);
+// console.log(reversedString); 
