930d2c4deffc4f56bdd80ead16da3c6c22130d7c
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-03-10 19:24:23 +0900

    fix: create checkAuthSession api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,6 +3,7 @@ import {T} from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum"
+import { Message } from "../libs/Errors";
 
 const restaurantController: T  = {};
 
@@ -69,6 +70,19 @@ restaurantController.processLogin = async (req: AdminRequest, res: Response) => 
              res.send(result);
          });
  
+    } catch (err) {
+        console.log("Error, processLogin:", err);
+        res.send(err);
+    }
+
+};
+
+restaurantController.checkAuthSession = async (req: AdminRequest, res: Response) => {
+    try{
+        console.log("checkAuthSession");
+        if(req.session?.member) 
+        res.send(`<script> alert(" ${req.session.member.memberNick}")</script>`);
+        else res.send(`<script> alert("${Message.NOT_AUTHENTOCATED}")</script>`);
     } catch (err) {
         console.log("Error, processLogin:", err);
         res.send(err);
