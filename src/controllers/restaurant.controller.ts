2bccde0d9640c14eaf2228352bb931e4e78ce7b9
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-03-17 21:55:28 +0900

    feat: develop getUsers business logic

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -99,6 +99,27 @@ restaurantController.logout = async (req: AdminRequest, res: Response) => {
 
 };
 
+restaurantController.getUsers = async (req: Request, res: Response) => {
+    try{
+        console.log("getUsers");
+        const result = await memberService.getUsers();
+        console.log("result", result);
+        res.render("users", { users: result });
+    } catch (err) {
+        console.log("Error, getUsers:", err);
+        res.redirect("/admin/login");
+    }
+};
+
+restaurantController.updatechosenUsers = (req: Request, res: Response) => {
+    try{
+        console.log("updatechosenUsers");
+        res.render("signup");
+    } catch (err) {
+        console.log("Error, updatechosenUsers:", err);
+        res.redirect("/admin");
+    }
+};
 restaurantController.checkAuthSession = async (req: AdminRequest, res: Response) => {
     try{
         console.log("checkAuthSession");
@@ -123,5 +144,7 @@ restaurantController.verifyRestaurant = (req: AdminRequest, res: Response, next:
     }
 
 };
+
+
 
 export default restaurantController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -36,4 +36,6 @@ routerAdmin.post(
 
 
 /** User */
+routerAdmin.get("/user/all", restaurantController.verifyRestaurant, restaurantController.getUsers)
+
 export default routerAdmin;
