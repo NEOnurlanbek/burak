c7dfb100019bbefff2d028bd3e2336cfa20d83b9
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-03-22 22:02:23 +0900

    fix: modify spa login api process

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -15,6 +15,7 @@ export enum Message {
     CREATE_FAILED = "Create is failed!",
     USED_NICK_PHONE = "You are inserting already used nick or phone!",
     NO_MEMBER_NICK = "No member with that member nick!",
+    BLOCKED_USER = "You have been blocked, contact restaurant!",
     WRONG_PASSWORD = "Wrong password intered, please try again!",
     NOT_AUTHENTOCATED = "You are  not authenticated, Plase login fist!",
     UPDATE_FAILED = "SIZ HATO QILDIZ"
