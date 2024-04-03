29a30c6dde2fd5d263b4a9287afbb0efc3705f41
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-04-03 17:42:41 +0900

    feat: T - task

--- src/train.ts
+++ src/train.ts
@@ -12,6 +12,40 @@ function toqsonlar(n: number) {
 
 console.log(toqsonlar(9)); 
 console.log(toqsonlar(11)); 
+
+//T -task
+function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+    let mergedArray: number[] = [];
+
+    let i: number = 0;
+    let j: number = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray.push(arr1[i]);
+            i++;
+        } else {
+            mergedArray.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        mergedArray.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        mergedArray.push(arr2[j]);
+        j++;
+    }
+
+    return mergedArray;
+}
+
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30])); 
+
  
 // Q - task
 // function calculate(yozuv: string) {
