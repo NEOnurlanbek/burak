7c289084ed81fdab75800a716d042a5703eecc89
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-04-11 20:07:43 +0900

    feat: develop getProducts rest Api

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader"
+import productController from "./controllers/product.controller";
 
 /** MEMBER */
 router.get("/member/restaurant", memberController.getRestaurant)
@@ -17,5 +18,7 @@ router.post("/member/update", memberController.verifyAuth, uploader("members").s
 router.get("/member/top-users", memberController.getTopUsers);
 
 /** PRODUCT*/
+
+router.get("/product/all/", productController.getProducts);
 
 export default router; 
